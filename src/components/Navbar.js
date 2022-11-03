a75d99b3814cfa46735a62029975f2d183831aae
AbrahamupSky <abrahamup7@gmail.com>
2022-11-02 21:36:33 -0600

    inicio

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Register.css"
+import logo from '../images/logo-nav.svg'
+
+function Navbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
