73cd52d532d90697d1ae09505ab5c6909caa2837
AbrahamupSky <abrahamup7@gmail.com>
2022-11-07 19:00:01 -0600

    navbar en proceso

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,32 @@
 import React from 'react'
-import "./Register.css"
-import logo from '../images/logo-nav.svg'
+import "./Navbar.css"
+import logo from '../images/logo-nav.png'
 import dashboard from '../images/dashboard.svg'
 import user from '../images/user.svg'
 
 function Navbar() {
     return (
-        <nav className='nav'>
-            <ul className='list'>
-                <li className='list__item'>
-                    <div className='div__button'>
-                        <img src={dashboard} className='list__img'/>
-                        <a href="#" className='nav__link'>Inicio</a>
-                    </div>
-                </li>
+       <header>
+             <a href="#" className='logo'><img src={logo}/></a>
 
-                <li className='list__item'>
-                    <div className='list__button list__button--click'>
-                        <img src={user} className='list__img'/>
-                        <a href="#" className='nav__link'>Usuario</a>
+            <ul className='navbar'>
+                <li><a href="#" className='active'>Usuario</a></li>
+                <li><a href="#">Viajes</a></li>
+                <li><a href="#">Mensualidad</a></li>
+                <li><a href="#">Contactar</a></li>
+                <li><a href="#">Ayuda</a></li>
+                
+                <div className='main'>
+                    <a href="#" className='user'><img src={user}/>Sign In</a>
+                    <a href="#">Register</a>
+
+                    <div className='bx bx-menu' id='menu-icon'>
+                        
                     </div>
-                </li>
+                </div>
             </ul>
-        </nav>
-    )
+       </header>
+    );
 }
 
 export default Navbar
